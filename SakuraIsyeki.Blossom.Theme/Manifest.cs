3b2de92cf22e051ed5ea19944508a0f893b2e50a
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-08-04 19:50:41 +0200

    Change solution directory structure + Init theme project

---
+++ SakuraIsyeki.Blossom.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "SakuraIsyeki.Blossom.Theme",
+    Author = "Sakura Akeno Isayeki",
+    Website = "https://sakura.ovh",
+    Version = "1.0",
+    Description = "Orchard Core Theme for The Blossom blog"
+)]
